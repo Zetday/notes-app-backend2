536f7c21c7b76164f73f4bc200a63979eccb1928
Zetday <muhammad.rendy.07@gmail.com>
2026-03-22 13:43:38 +0800

    add: menambahkan validasi data pada query params

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -6,16 +6,17 @@ import {
   editNoteById,
   deleteNoteById,
 } from '../controller/note-controller.js';
-import validate from '../../../middlewares/validate.js';
+import { validate, validateQuery } from '../../../middlewares/validate.js';
 import {
   notePayloadSchema,
   noteUpdatePayloadSchema,
+  noteQuerySchema,
 } from '../../../services/notes/validator/schema.js';
 
 const router = express.Router();
 
 router.post('/notes', validate(notePayloadSchema), createNote);
-router.get('/notes', getNotes);
+router.get('/notes', validateQuery(noteQuerySchema), getNotes);
 router.get('/notes/:id', getNoteById);
 router.put('/notes/:id', validate(noteUpdatePayloadSchema), editNoteById);
 router.delete('/notes/:id', deleteNoteById);
