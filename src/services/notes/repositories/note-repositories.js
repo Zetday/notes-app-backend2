86649790d1d76465c82587993f95b157cbb9ee20
Zetday <muhammad.rendy.07@gmail.com>
2026-03-23 21:10:26 +0800

    add: membuat fungsi editNoteById, dan deleteNoteById

--- src/services/notes/repositories/note-repositories.js
+++ src/services/notes/repositories/note-repositories.js
@@ -36,4 +36,30 @@ class NoteRepositories {
 
     return result.rows[0];
   }
+
+  async editNote({ id, title, body, tags }) {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+      values: [title, body, tags, updatedAt, id],
+    };
+
+    const result = await this.pool.query(query);
+
+    return result.rows[0];
+  }
+
+  async deleteNote(id) {
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+
+    return result.rows[0].id;
+  }
 }
+
+export default new NoteRepositories();
