b1ad81e029602761c9884fabb258664d49f779d5
Zetday <muhammad.rendy.07@gmail.com>
2026-04-05 13:48:51 +0800

    feat: menambahkan uploads controller

--- src/services/uploads/routes/index.js
+++ src/services/uploads/routes/index.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import { uploadImages } from '../controllers/upload-controller.js';
-import authenticateToken from '../../../middlewares/auth.js';
+import { uploadImages } from '../controller/upload-controller.js';
 import { upload } from '../storage/storage-config.js';
 
 const router = Router();
 
-router.post(
-  '/upload/images',
-  authenticateToken,
-  upload.single('image'),
-  uploadImages,
-);
+router.post('/upload/images', upload.single('image'), uploadImages);
 
 export default router;
