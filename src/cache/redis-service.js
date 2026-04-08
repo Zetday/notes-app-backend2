afa2a03e443c6c7ddb57f11d87fa30533ffa63ed
Zetday <muhammad.rendy.07@gmail.com>
2026-04-08 20:23:37 +0800

    feat: membuat fungsi CacheService.set

---
+++ src/cache/redis-service.js
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+
+class CacheService {
+  constructor() {
+    this._client = createClient({
+      socket: {
+        host: process.env.REDIS_SERVER,
+      },
+    });
+
+    this._client.on('error', (error) => {
+      console.error(error);
+    });
+
+    this._client.connect();
+  }
+
+  async set(key, value, expirationInSecond = 3600) {
+    await this._client.set(key, value, {
+      EX: expirationInSecond,
+    });
+  }
+}
